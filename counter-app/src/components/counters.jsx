01720a2d6d9d1a57c81073b04a9eb589ccb429c9
Venkatesh D <venkigd8@gmail.com>
2021-10-16 12:43:41 +0530

    Single source of truth

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -18,10 +18,23 @@ class Counters extends React.Component {
         //update the state of counters to the newCounters 
         //overwrite counters property with newCounters constant
         this.setState({counters: newCounters});
-    }
+    };
+
+    //creating new array of counters and set state
+    handleReset = () =>{
+        const newCounters = this.state.counters.map(c=>{
+            c.value = 0;
+            return c});
+        this.setState({counters : newCounters});
+    };
 
     render() { 
         return (<div>
+                <button 
+                onClick={this.handleReset}
+                className="btn btn-primary btn-sm m-2">
+                    Reset
+                </button>
             {this.state.counters.map(counter => 
             <Counter 
                 key={counter.id} 
