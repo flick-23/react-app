eaeaeb45768cfce5cd3fd3fab495344ba37b6d90
Venkatesh D <venkigd8@gmail.com>
2021-10-22 12:58:56 +0530

    Pagination -Type checking with PropTypes

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import _ from 'lodash';        
+import React, { Component } from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+
 const Pagination = (props) => {
-    //[1,... pagesCount].map()
-    const {itemsCount, pageSize, currentPage, onPageChange} = props;
-    console.log(currentPage);
-    const pagesCount = Math.ceil(itemsCount/pageSize);
-    console.log("Page count : ",pagesCount);
-    if(pagesCount === 1) return null;
-    const pages = _.range(1,pagesCount+1)
-    return <nav>
-        <ul className="pagination">
-            {pages.map(page=>(
-                <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
-                    <a className="page-link" 
-                        onClick={()=>onPageChange(page)}
-                    >{page}</a>
-                </li>
-            ))
-            }
-        </ul>
-    </nav>;
-}
- 
+  //[1,... pagesCount].map()
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  console.log("Page count : ", pagesCount);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+};
 export default Pagination;
