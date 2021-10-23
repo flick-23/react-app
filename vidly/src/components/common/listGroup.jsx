c962cfcfb04b293fa1f4eee518812977e8eeb098
Venkatesh D <venkigd8@gmail.com>
2021-10-23 20:48:03 +0530

    Solution-2-Filtering Display Names

--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 const ListGroup = (props) => {
-  return null;
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
