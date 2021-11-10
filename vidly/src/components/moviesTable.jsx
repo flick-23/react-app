783d731d74ee8f7b37a4dd1f9de7b2a9d8adb8b4
Venkatesh D <venkigd8@gmail.com>
2021-11-10 21:11:08 +0530

    Sorting -Extracting Table

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import Like from "./common/like";
 import React, { Component } from "react";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody";
+import Table from "./common/table";
 class MoviesTable extends Component {
   columns = [
     { path: "title", label: "Title" },
@@ -31,14 +30,12 @@ class MoviesTable extends Component {
     const { movies, onSort, sortColumn } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody columns={this.columns} data={movies} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
