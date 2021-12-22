1440cc5917415531870fdb9c7731ba39ca6530fa
Venkatesh D <venkigd8@gmail.com>
2021-12-22 13:00:46 +0530

    Adding Routes

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,10 +1,21 @@
-import React, {Component} from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React, { Component } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Routes>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/customers" element={<Customers />}></Route>
+        <Route path="/rentals" element={<Rentals />}></Route>
+        <Route path="/not-found" element={<NotFound />}></Route>
+        <Route path="/" element={<Navigate to="movies" />} />
+        <Route path="*" element={<Navigate to="not-found" />} />
+      </Routes>
     </main>
   );
 }
