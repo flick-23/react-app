e29819f2ad1c1c849192ec24bceee90b1d8ba174
Venkatesh D <venkigd8@gmail.com>
2021-12-22 13:21:03 +0530

    Adding the Navbar

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 function App() {
   return (
-    <main className="container">
-      <Routes>
-        <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/customers" element={<Customers />}></Route>
-        <Route path="/rentals" element={<Rentals />}></Route>
-        <Route path="/not-found" element={<NotFound />}></Route>
-        <Route path="/" element={<Navigate to="movies" />} />
-        <Route path="*" element={<Navigate to="not-found" />} />
-      </Routes>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Routes>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/customers" element={<Customers />}></Route>
+          <Route path="/rentals" element={<Rentals />}></Route>
+          <Route path="/not-found" element={<NotFound />}></Route>
+          <Route path="/" element={<Navigate to="movies" />} />
+          <Route path="*" element={<Navigate to="not-found" />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
