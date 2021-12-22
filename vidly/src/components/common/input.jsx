56132f8357b2943c1546231c6413e230736491a2
Venkatesh D <venkigd8@gmail.com>
2021-12-22 19:55:40 +0530

    Extracting helper rendering methods

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-const Input = ({ error, name, label, value, onChange }) => {
+const Input = ({ error, name, label, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alter-danger">{error}</div>}
     </div>
   );
