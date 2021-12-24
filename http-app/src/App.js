6c555418263bd4e7dc738da74197d37cfd5f878d
Venkatesh D <venkigd8@gmail.com>
2021-12-24 23:23:19 +0530

    Extracting a config module

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
+import config from "./config.json";
 import http from "./services/httpService";
 
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
     posts: [],
@@ -10,22 +10,22 @@ class App extends Component {
 
   async componentDidMount() {
     //pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await http.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async (post) => {
     post.title = "UPDATED";
-    const { data } = await http.put(apiEndpoint + "/" + post.id, post);
+    const { data } = await http.put(config.apiEndpoint + "/" + post.id, post);
     //OR do patch for selected objs
-    // http.patch(apiEndpoint + "/" + post.id, { title: post.title });
+    // http.patch(config.apiEndpoint + "/" + post.id, { title: post.title });
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...post };
@@ -41,7 +41,7 @@ class App extends Component {
     this.setState({ posts });
     //Update server in try catch block
     try {
-      await http.delete(apiEndpoint + "/" + post.id);
+      await http.delete(config.apiEndpoint + "/" + post.id);
       // throw new Error(""); just for demostrating errors
     } catch (ex) {
       console.log("HANDLE DELETE CATCH BLOCK!");
